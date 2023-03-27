d74c81843f6b7dd37dd635c35a9c68cc1e7a0218
liu.ziwen <liu.ziwen@cn.fujitsu.com>
2023-03-27 10:11:48 +0900

    feat: 3-27

--- src/main/java/com/zero/basic/HelloWorld.java
+++ src/main/java/com/zero/basic/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.zero.basic;
 
+import java.util.Date;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.err.println("Hello World");
+		System.err.println(new Date());
+		System.err.println(new Date().toString());
 	}
 }
